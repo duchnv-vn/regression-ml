5d3fca80009748d8c91e7f0fbcad885e346503b1
SICKCN\huynhdu1 <duc.huynh@sick.com>
2025-05-30 23:41:41 +0700

    feat: prepare mnist data for a digit clarification in images

---
+++ mnist/load_image.py
@@ -0,0 +1,26 @@
+import gzip
+import struct
+
+import numpy as np
+
+
+def load_images(filename):
+    with gzip.open(filename, 'rb') as f:
+        _, n_images, columns, rows = struct.unpack('>IIII', f.read(16))
+        all_pixels = np.frombuffer(f.read(), dtype=np.uint8)
+        return all_pixels.reshape(n_images, columns * rows)
+
+
+def prepend_bias(X):
+    return np.insert(X, 0, 1, axis=1)
+
+
+def load_label(filename):
+    with gzip.open(filename, 'rb') as f:
+        f.read(8)
+        all_labels = f.read()
+        return np.frombuffer(all_labels, dtype=np.uint8).reshape(-1, 1)
+
+
+def encode_target_label(Y, target):
+    return (Y == target).astype(int)
